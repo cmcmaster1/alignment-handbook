c93c0d461bc485366ad1f0b5ef08fbdff796f72e
Chris McMaster <mcmastc1@gmail.com>
2023-11-25 21:24:10 +1100

    turn off flash attention :(

--- finetune.sh
+++ finetune.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+export HF_HOME="/data/gpfs/projects/punim1509/rheum_model/alignment-handbook/.cache"
 ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/deepspeed_zero3.yaml scripts/run_pretrain.py recipes/zephyr-7b-beta/pretrain/config_full.yaml
